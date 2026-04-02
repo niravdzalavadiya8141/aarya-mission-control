32dddffa0ffaea920f1f44912abb08291e10ff35
niravdzalavadiya8141 <niravdzalavadiya@gmail.com>
2026-04-02 17:00:59 +0530

    [BUCKY] feat: polish, responsive, loading screen, 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Gamification from './pages/Gamification';
+import Settings from './pages/Settings';
 // other imports
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/xp" component={Gamification} />
+        <Route path="/settings" component={Settings} />
         // other routes
       </Switch>
     </Router>
